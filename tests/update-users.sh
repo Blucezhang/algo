ddf4d55a55e873883fcd58a6cfd536b2f88b6a45
Jack Ivanov <e601809@gmail.com>
2018-04-30 13:55:40 +0300

    Refactoring, booleans declaration and update users fix

--- tests/update-users.sh
+++ tests/update-users.sh
@@ -2,14 +2,11 @@
 
 set -ex
 
-CAPW=`cat /tmp/ca_password`
-USER_ARGS="server_ip=$LXC_IP server_user=ubuntu ssh_tunneling_enabled=y IP_subject=$LXC_IP easyrsa_CA_password=$CAPW"
-
-sed -i 's/- jack$/- jack_test/' config.cfg
+USER_ARGS="server=$LXC_IP users=['user1', 'user2']"
 
 if [ "${LXC_NAME}" == "docker" ]
 then
-  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -e "USER_ARGS=${USER_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook users.yml -e \"${USER_ARGS}\""
+  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/algo/configs -e "USER_ARGS=${USER_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook users.yml -e \"${USER_ARGS}\""
 else
   ansible-playbook users.yml -e "${USER_ARGS}"
 fi
@@ -24,7 +21,7 @@ if openssl crl -inform pem -noout -text -in crl/jack.crt | grep CRL
     exit 1
 fi
 
-if openssl x509 -inform pem -noout -text -in certs/jack_test.crt | grep CN=jack_test
+if openssl x509 -inform pem -noout -text -in certs/user1.crt | grep CN=user1
   then
     echo "The new user exists"
   else
