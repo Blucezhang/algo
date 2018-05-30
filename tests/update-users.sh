51113244b195c8abfcc8382ec93f25a70fd98088
Jack Ivanov <e601809@gmail.com>
2018-05-30 21:42:46 +0300

    add debug

--- tests/update-users.sh
+++ tests/update-users.sh
@@ -6,7 +6,7 @@ USER_ARGS="server=$LXC_IP users=['user1', 'user2']"
 
 if [ "${LXC_NAME}" == "docker" ]
 then
-  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/algo/configs -e "USER_ARGS=${USER_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook users.yml -e \"${USER_ARGS}\""
+  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/algo/configs -e "USER_ARGS=${USER_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook users.yml -vvvv -e \"${USER_ARGS}\""
 else
   ansible-playbook users.yml -e "${USER_ARGS}"
 fi
